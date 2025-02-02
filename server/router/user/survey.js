b658e89072375f741d087e7e7ac03056efdc9fd8
lvnko <kevinlao.lo@gmail.com>
2025-02-02 15:22:19 -0300

    完成了 User's Surveys 的 GET API 開發。

--- server/router/user/survey.js
+++ server/router/user/survey.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require('path');
 const mongoose = require("mongoose");
+const User = require(path.join(__dirname, '../../models/User'));
 const Survey = require(path.join(__dirname, '../../models/Survey'));
 const router = express.Router();
-const { standardErrorHandlerOnPost, customErrorHandler, getFullReqUrl, getReqBodyDataAsModelSchema, validateObjectId } = require(path.join(__dirname, '../../utilities'));
+const { standardErrorHandler, standardErrorHandlerOnPost, customErrorHandler, getFullReqUrl, getReqBodyDataAsModelSchema, validateObjectId } = require(path.join(__dirname, '../../utilities'));
 
 router.post('/', async (req, res) => {
 
@@ -35,16 +36,54 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify({
-        status: "success",
-        message: "Testing is succeeded.",
-        // data: {
-        //     user: result
-        // }
-    }));
-    res.end();
+
+    try {
+        const params = { ...req.parentParams, ...req.params };
+        validateObjectId(params.user_id, `It requires a valid User ID to find a user.`);
+        const authorId = new mongoose.Types.ObjectId(`${params.user_id}`);
+        
+        const results = await User.aggregate([
+            {
+                $match: { _id: authorId }
+            },{
+                $limit: 1
+            },{
+                $lookup: {
+                    from: "surveys",
+                    localField: "_id",
+                    foreignField: "authorId",
+                    as: "surveys"
+                }
+            },{
+                $project: {
+                    "surveys.authorId": 0
+                }
+            }
+        ]);
+
+        console.log('results =>', results);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({
+            status: results.length > 0 ? "success" : "fail",
+            message: results.length > 0 ?
+                results[0].surveys.length > 0 ?
+                    "We found these surveys that are created by this user." : "We found this user has created no survey." :
+                "There is no survey nor a user is found.",
+            data: {
+                user: results.length > 0 ? results[0] : null
+            }
+        }));
+        res.end();
+    } catch (error) {
+        return standardErrorHandler(res, {
+            status: error.message.indexOf('fail') >= 0 ? "fail" : "error",
+            code: error.message.indexOf('fail') >= 0 ? 400 : 404,
+            message: error.message
+        });
+    }
 });
+
 router.get('/:survey_id', async (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.write(JSON.stringify({
@@ -60,6 +99,7 @@ router.get('/:survey_id', async (req, res) => {
     }));
     res.end();
 });
+
 router.put('/:survey_id', async (req, res) => {});
 router.delete('/:survey_id', async (req, res) => {});
 
