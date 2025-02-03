0afb110d6bf176223f63566016eed6fcba401dd7
lvnko <kevinlao.lo@gmail.com>
2025-02-02 21:01:38 -0300

    完成 GET 指定 User 的指定 Survey API Endpoint 開發。

--- server/router/user/survey.js
+++ server/router/user/survey.js
@@ -61,8 +61,6 @@ router.get('/', async (req, res) => {
             }
         ]);
 
-        console.log('results =>', results);
-
         res.setHeader('Content-Type', 'application/json');
         res.write(JSON.stringify({
             status: results.length > 0 ? "success" : "fail",
@@ -85,22 +83,87 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:survey_id', async (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.write(JSON.stringify({
-        status: "success",
-        message: "Testing is succeeded.",
-        params: {
-            ...req.params,
-            ...(req.parentParams ? req.parentParams : {})
-        }
-        // data: {
-        //     user: result
-        // }
-    }));
-    res.end();
+    try {
+
+        const params = { ...req.parentParams, ...req.params };
+        validateObjectId(params.user_id, `It requires a valid User ID to find a user.`);
+        validateObjectId(params.survey_id, `It requires a valid Survey ID to find a survey.`);
+        const authorId = new mongoose.Types.ObjectId(`${params.user_id}`);
+        const surveyId = new mongoose.Types.ObjectId(`${params.survey_id}`);
+
+        const results = await Survey.aggregate([
+            {
+                $match: { _id: surveyId, authorId: authorId }
+            },{
+                $limit: 1
+            },{
+                $lookup: {
+                    from: "users",
+                    localField: "authorId",
+                    foreignField: "_id",
+                    as: "author"
+                }
+            },{
+                $project:  {
+                    title: 1,
+                    body: 1,
+                    status: 1,
+                    minPairAppearance: 1,
+                    highestSingleAppearance: 1,
+                    voteCountEachSurvey: 1,
+                    fullfilled: 1,
+                    updatedAt: 1,
+                    createdAt: 1,
+                    author: {
+                        $arrayElemAt: ["$author", 0],
+                    }
+                }
+            },{
+                $project: {
+                    author: {
+                        "dateOfBirth": 0,
+                        "email": 0,
+                        "mobileNum": 0,
+                        "mobileCountryCode": 0,
+                        "subscription": 0,
+                        "updatedAt": 0,
+                        "createdAt": 0,
+                        "__v": 0,
+                    }
+                }
+            }
+        ]);
+
+        const result = results.length > 0 ? results[0] : null;
+
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify({
+            ...result === null ? {
+                status: "fail",
+                message: "We found nothing from this user."
+            } : {
+                status: "success",
+                messsage: "We found a survey that was created by this user."
+            },
+            data: {
+                survey: result
+            }
+        }));
+        res.end();
+
+    } catch (error) {
+
+        return standardErrorHandler(res, {
+            status: error.message.indexOf('fail') >= 0 ? "fail" : "error",
+            code: error.message.indexOf('fail') >= 0 ? 400 : 404,
+            message: error.message
+        });
+
+    }
 });
 
 router.put('/:survey_id', async (req, res) => {});
+
 router.delete('/:survey_id', async (req, res) => {});
 
 module.exports = router;
