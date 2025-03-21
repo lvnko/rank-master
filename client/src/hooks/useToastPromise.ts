e2614fdd5fe2a99789be42a80f1bf12cef1a541a
lvnko <kevinlao.lo@gmail.com>
2025-03-21 12:19:18 -0300

    改進 ToastPromise

--- client/src/hooks/useToastPromise.ts
+++ client/src/hooks/useToastPromise.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
 import { toast } from 'sonner';
 
 export interface ToastMessage {
@@ -5,50 +7,98 @@ export interface ToastMessage {
     description: string
 };
 
-const useToastPromise = () => {
+interface Labels {
+    loading?: string;
+}
 
-    const toastPromise = async <T extends any[]> (
-        promise: (...args: T) => Promise<any>,
-        args: T,
-        successMessage: ToastMessage | ((data: any) => ToastMessage),
-        callback?: (data: any) => void
-    ) => {
+const useToastPromise = ({ labels }: { labels: Labels }) => {
 
-        const toastId = toast.loading('Loading...');
+    // const toastPromise = async <T extends any[]> (
+    //     promise: (...args: T) => Promise<any>,
+    //     args: T,
+    //     successMessage: ToastMessage | ((data: any) => ToastMessage),
+    //     callback?: (data: any) => void
+    // ) => {
 
-        try {
-            const data = await promise(...args);
-            toast.dismiss(toastId);
-            const successMsg = typeof successMessage === 'function' ? successMessage(data) : successMessage;
-            toast.success(
-                ...[
-                    typeof successMsg === 'string' ?
-                        successMsg :
-                        successMsg.message,
-                    {   
-                        id: toastId,
-                        ...(
-                            typeof successMsg === 'object' && successMsg.description ?
-                                { description: successMsg.description } : null
-                        )
-                    }
-                ]
-            );
+    //     // const toastId = toast.loading('Loading...');
 
-            if (callback) {
-                callback(data);
+    //     // try {
+    //     //     const data = await promise(...args);
+    //     //     toast.dismiss(toastId);
+    //     //     const successMsg = typeof successMessage === 'function' ? successMessage(data) : successMessage;
+    //     //     toast.success(
+    //     //         ...[
+    //     //             typeof successMsg === 'string' ?
+    //     //                 successMsg :
+    //     //                 successMsg.message,
+    //     //             {   
+    //     //                 id: toastId,
+    //     //                 ...(
+    //     //                     typeof successMsg === 'object' && successMsg.description ?
+    //     //                         { description: successMsg.description } : null
+    //     //                 )
+    //     //             }
+    //     //         ]
+    //     //     );
+
+    //     //     if (callback) {
+    //     //         callback(data);
+    //     //     }
+
+    //     // } catch (error) {
+
+    //     //     console.error('Error:', error);
+    //     //     toast.error('Error', {id: toastId});
+
+    //     //     if (callback) {
+    //     //         callback(error);
+    //     //     }
+    //     // }
+
+    // };
+
+    const { t } = useTranslation();
+    const loadingLabelDefaultValue = labels.loading || t('loading', { ns: 'common' });
+    const successLabelDefaultValue = labels.loading || t('loading', { ns: 'common' });
+    const errorLabelDefaultValue = labels.loading || t('loading', { ns: 'common' });
+
+    const toastPromise = <T extends any[]> ({
+            promise,
+            args,
+            loadingMessage,
+            successMessage = successLabelDefaultValue,
+            errorMessage = errorLabelDefaultValue,
+            callback,
+            errorCallback
+        }: {
+            promise: (...args: T) => Promise<any>,
+            args: T,
+            loadingMessage: string,
+            successMessage: string | ToastMessage | ((data: any) => string | ToastMessage),
+            errorMessage: string | ((error: any) => string),
+            callback?: (data?: any) => void,
+            errorCallback?: (error?: any) => void
+    }) => {
+        toast.promise(
+            promise(...args),
+            {
+                loading: loadingLabelDefaultValue,
+                success: (resData) => {
+                    console.log('resData =>', resData);
+                    if (callback) callback(resData);
+                    const successMsg = typeof successMessage === 'function' ? successMessage(resData) : String(successMessage);
+                    return typeof successMsg === 'object' ? 
+                            { message: successMsg.message, description: successMsg.description } :
+                            { message: String(successMsg) };
+                },
+                error: (error) => {
+                    console.log('error =>', error);
+                    if (errorCallback) errorCallback(error);
+                    const errorMsg = typeof errorMessage === 'function' ? errorMessage(error) : errorMessage;
+                    return errorMsg;
+                },
             }
-
-        } catch (error) {
-
-            console.error('Error:', error);
-            toast.error('Error', {id: toastId});
-
-            if (callback) {
-                callback(error);
-            }
-        }
-
+        );
     };
 
     return toastPromise;
